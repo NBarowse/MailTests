e7b5bd3d15d47046e868203430d3a2db4c042972
Anastasiya Ramanovich <Anastasiya_Ramanovich@epam.com>
2019-01-22 11:33:19 +0300

    created Base Test class

---
+++ 5 Selenium Webdriver/Tests/BaseGmailTest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using System.Configuration;
+using OpenQA.Selenium.Support.UI;
+
+namespace Selenium_Webdriver
+{
+    [TestClass]
+    public class BaseGmailTest
+    {
+        protected IWebDriver driver;
+
+        #region Variables
+        private string baseUrl = "https://www.gmail.com/";
+        private string browser = ConfigurationManager.AppSettings["browser"];
+        private string mail = ConfigurationManager.AppSettings["mail"];
+        private string pswrd = ConfigurationManager.AppSettings["pswrd"];
+        #endregion
+
+        #region Locators
+        private By username = By.Id("identifierId");
+        private By usernameNext = By.Id("identifierNext");
+        private By password = By.Name("password");
+        private By passwordNext = By.Id("passwordNext");
+        protected By profileIcon = By.XPath("//*[@class='gb_cb gbii']");
+        protected By btnExit = By.XPath("//a[text()='Выйти']");
+        protected By btnWrite = By.XPath("//div[@role='button' and contains(.,'Написать')]");
+        protected By btnCloseDraft = By.CssSelector("*[alt='Закрыть']");
+        protected By btnSend = By.XPath("//div[@role='button' and contains(.,'Отправить')]");
+        protected By nameBox = By.XPath("//*[@name='to']");
+        protected By subjectBox = By.CssSelector("input[name='subjectbox']");
+        protected By subjectInDraft = By.XPath("//*[@class='aYF']");
+        protected By bodyBox = By.CssSelector("div[aria-label='Тело письма']");
+        protected By drafts = By.XPath("//*[@title='Черновики']");
+        protected By sent = By.XPath("//*[@title='Отправленные']");
+        #endregion
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            if ("ff".Equals(this.browser))
+            {
+                var service = FirefoxDriverService.CreateDefaultService();
+                this.driver = new FirefoxDriver(service);
+            }
+            else
+            {
+                ChromeOptions option = new ChromeOptions();
+                option.AddArgument("disable-infobars");
+                this.driver = new ChromeDriver(option);
+            }
+
+            this.driver.Navigate().GoToUrl(this.baseUrl);
+            this.driver.Manage().Window.Maximize();
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            this.driver.Close();
+            this.driver.Quit();
+        }
+
+        protected void SignIn()
+        {
+            WaitForElement(username);
+            this.driver.FindElement(username).SendKeys(mail);
+            this.driver.FindElement(usernameNext).Click();
+
+            WaitForElement(password);
+            this.driver.FindElement(password).SendKeys(pswrd);
+            this.driver.FindElement(passwordNext).Click();
+        }
+
+        protected void LogOut()
+        {
+            this.driver.FindElement(profileIcon).Click();
+            WaitForElement(btnExit);
+            this.driver.FindElement(btnExit).Click();
+        }
+
+        protected void WaitForElement(By locatorKey, int timeoutSecs = 20)
+        {
+            new WebDriverWait(this.driver, TimeSpan.FromSeconds(timeoutSecs)).Until(ExpectedConditions.ElementIsVisible(locatorKey));
+        }
+
+        protected bool IsElementPresent(By locatorKey)
+        {
+            try
+            {
+                if (this.driver.FindElement(locatorKey).Displayed) ;
+                return true;
+            }
+            catch (NoSuchElementException e)
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
---
+++ 5 Selenium Webdriver/Tests/GmailTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Firefox;
+
+namespace Selenium_Webdriver
+{
+    [TestClass]
+    public class GmailTest : BaseGmailTest
+    {
+        [TestMethod]
+        public void TestSendEmail()
+        {
+            #region Variables
+            string mailTo = "anastasiya.maniak@gmail.com";
+            string subject = "Subject " + DateTime.Now;
+            string text = "Text...";
+            #endregion
+
+            #region Locators
+            By myDraft = By.XPath("//*[@role='main']//div[@role='link']//span[contains(.,'" + subject + "')]/span");
+            #endregion
+
+            //•	Login to the mail box.
+            SignIn();
+
+            //•	Assert, that the login is successful: Если появляется кнопка Написать, значит вход выполнен успешно
+            WaitForElement(btnWrite);
+            StringAssert.Contains(this.driver.FindElement(btnWrite).Text, "Написать", "Login failed.");
+
+            //•	Create a new mail(fill addressee, subject and body fields).
+            this.driver.FindElement(btnWrite).Click();
+            WaitForElement(nameBox);
+            this.driver.FindElement(nameBox).SendKeys(mailTo);
+            this.driver.FindElement(subjectBox).SendKeys(subject);
+            this.driver.FindElement(bodyBox).SendKeys(text);
+
+            //•	Save the mail as a draft.
+            this.driver.FindElement(btnCloseDraft).Click();
+
+            //•	Verify, that the mail presents in ‘Drafts’ folder.
+            this.driver.SwitchTo().DefaultContent();
+            WaitForElement(drafts);
+            this.driver.FindElement(drafts).Click();
+            WaitForElement(myDraft);
+            Assert.AreEqual(this.driver.FindElement(myDraft).Text, subject, "Draft wasn't saved");
+
+            //•	Verify the draft content(addressee, subject and body – should be the same as in 3  
+            this.driver.FindElement(myDraft).Click();
+
+            WaitForElement(bodyBox);
+            StringAssert.Contains(this.driver.FindElement(nameBox).GetAttribute("value"), mailTo, "TO value is not expected");
+            StringAssert.Contains(this.driver.FindElement(subjectInDraft).Text, subject, "Subject value is not expected");
+            StringAssert.Contains(this.driver.FindElement(bodyBox).Text, text, "Body value is not expected");
+
+            //•	Send the mail.
+            this.driver.FindElement(btnSend).Click();
+
+            //•	Verify, that the mail disappeared from ‘Drafts’ folder.
+            this.driver.SwitchTo().DefaultContent();
+            this.driver.Navigate().Refresh();
+            WaitForElement(drafts);
+            Assert.IsFalse(IsElementPresent(myDraft));
+
+            //•	Verify, that the mail is in ‘Sent’ folder
+            this.driver.FindElement(sent).Click();
+            WaitForElement(myDraft);
+            Assert.AreEqual(this.driver.FindElement(myDraft).Text, subject, "Mail is not in 'Sent' folder");
+
+            //•	Log off.
+            LogOut();
+        }
+
+
+
+    }
+}
